04e4335a7b62fc6b7db020c77d03abc7464ad68f
jlliller <jliller91@gmail.com>
2016-07-06 16:00:01 -0400

    created 8ball and added top level comment

---
+++ 8ball.py
@@ -0,0 +1,16 @@
+"""
+Justin Liller
+Magic 8 Ball Instructions:
+
+I'm sure you've used a magic 8 ball at one point in your life. You ask it a question, turn it right side up and it gives
+an answer by way of a floating die with responses written on it. You can create one in python. You must:
+    1. Allow the user to enter their question
+    2. Display an in progress message( i.e. "thinking")
+    3. Create 20 responses, and show a random response
+    4. Allow the user to ask another question or quit
+Bonus - Using whatever module you like, add a gui. Your gui must have:
+    1. A box for users to enter the question
+    2. At least 4 buttons: Ask , clear(the text box), play again and quit(this must close the window)
+
+7/6/2016
+"""
